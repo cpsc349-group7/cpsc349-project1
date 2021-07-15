cc2859d83f03ab7bbe22348960256dae58e5b451
David Bonnaud <dbonnaud123@gmail.com>
2021-07-14 17:36:23 -0700

    added captions below links and added css and centered.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
+    eleventyConfig.addPassthroughCopy('css')
+  return {
+    passthroughFileCopy: true
+  }
 };
